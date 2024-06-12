d6f7b1dc6b238c6c2570c9d485cb23b6af30f594
nicholasdoloksaribu <113407564+nicholasdoloksaribu@users.noreply.github.com>
2024-06-12 21:51:29 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function searchMovie(){
     $("#movie-list").html('');
     $.ajax({
-      url:"http://www.omdbapi.com",
+      url:"https://www.omdbapi.com",
       type:"GET",
       datatype:"JSON",
       data:{
